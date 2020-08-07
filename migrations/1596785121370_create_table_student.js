84d1bc136d2b6cac705b3d2bd40f5099cd06a0fc
Yeo Junkai <reginleiff@gmail.com>
2020-08-08 00:20:12 +0800

    feat: update migrations to handle duplicates

--- migrations/1596785121370_create_table_student.js
+++ migrations/1596785121370_create_table_student.js
@@ -1,5 +1,5 @@
 module.exports = {
   up:
-    'CREATE TABLE student (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(320) NOT NULL, is_suspended BOOLEAN DEFAULT false);',
+    'CREATE TABLE student (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(320) NOT NULL, is_suspended BOOLEAN DEFAULT false, UNIQUE (email));',
   down: 'DROP TABLE student;',
 };
--- migrations/1596787206081_create_table_registration.js
+++ migrations/1596787206081_create_table_registration.js
@@ -1,5 +1,5 @@
 module.exports = {
   up:
-    'CREATE TABLE registration(student_id BIGINT UNSIGNED NOT NULL REFERENCES student(id), teacher_id BIGINT UNSIGNED NOT NULL REFERENCES teacher(id));',
+    'CREATE TABLE registration(student_id BIGINT UNSIGNED NOT NULL REFERENCES student(id), teacher_id BIGINT UNSIGNED NOT NULL REFERENCES teacher(id), UNIQUE(student_id, teacher_id));',
   down: 'DROP TABLE registration;',
 };
